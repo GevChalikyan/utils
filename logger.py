ad7424d3b0c0da834e82c8b8aad98e1b281d2c02
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-10-01 06:37:37 -0700

    Changes Setup Logging Implementation
    Using a list of tuples now containing the log level and message to preserve order of logs instead of using log level as key for a dictionary

--- logger.py
+++ logger.py
@@ -2,25 +2,25 @@ import logging
 import os
 
 def setup():
+
   try:
     log_dir = '../logs'
     log_file = '../logs/app.log'
-    log_setup_info = {
-      logging.WARNING: [],
-      logging.INFO: []
-    }
+    log_setup_info = []
 
 
 
     if not os.path.isfile(log_file):
-      log_setup_info[logging.WARNING].append('Log file missing')
+      log_setup_info.append((logging.WARNING, 'Log file missing'))
+
       if not os.path.isdir(log_dir):
-        log_setup_info[logging.WARNING].append('Log directory missing')
+        log_setup_info.append((logging.WARNING, 'Log directory missing'))
+
         os.mkdir(log_dir)
-        log_setup_info[logging.INFO].append(f'Successfully created log directory at \'{log_dir}\'')
+        log_setup_info.append((logging.INFO, f'Successfully created log directory at \'{log_dir}\''))
 
       with open(log_file, 'w') as f:
-        log_setup_info[logging.INFO].append(f'Successfully created log file at \'{log_file}\'')
+        log_setup_info.append((logging.INFO, f'Successfully created log file at \'{log_file}\''))
 
 
 
@@ -33,9 +33,8 @@ def setup():
 
     
 
-    for log_level in log_setup_info:
-      for msg in log_setup_info[log_level]:
-        logging.log(level=log_level, msg=msg)
+    for log in log_setup_info:
+      logging.log(level=log[0], msg=log[1])
 
     logging.info('Successfully setup logger.')
 
