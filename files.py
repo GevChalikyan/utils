ec05ba3cd288794138aebff0a63f39157822f19d
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-10-02 23:16:19 -0700

    Added Logging

--- files.py
+++ files.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 def validate_dir(file_path):
@@ -5,3 +6,4 @@ def validate_dir(file_path):
   dir = os.path.dirname(file_path)
   if not os.path.isdir(dir):
     os.makedirs(dir)
+    logging.info(f'Created new directory: \'{dir}\'')
