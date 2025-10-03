d2f455c0d1bc078cc8768d87c4336840b90734ee
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-10-03 14:13:14 -0700

    Removed Setup Method
    Setup occurs automatically upon import

--- logger.py
+++ logger.py
@@ -1,43 +1,45 @@
 import logging
 import os
 
-def setup():
-
-  try:
-    log_dir = '../logs'
-    log_file = '../logs/app.log'
-    log_setup_info = []
 
 
 
-    if not os.path.isfile(log_file):
-      log_setup_info.append((logging.WARNING, 'Log file missing'))
 
-      if not os.path.isdir(log_dir):
-        log_setup_info.append((logging.WARNING, 'Log directory missing'))
-
-        os.mkdir(log_dir)
-        log_setup_info.append((logging.INFO, f'Successfully created log directory at \'{log_dir}\''))
-
-      with open(log_file, 'w') as f:
-        log_setup_info.append((logging.INFO, f'Successfully created log file at \'{log_file}\''))
+try:
+  log_dir = '../logs'
+  log_file = '../logs/app.log'
+  log_setup_info = []
 
 
 
-    logging.basicConfig(
-      filename=log_file, 
-      level=logging.INFO, 
-      filemode='w',
-      format='%(asctime)s | %(levelname)s:\n\t%(message)s'
-    )
+  if not os.path.isfile(log_file):
+    log_setup_info.append((logging.WARNING, 'Log file missing'))
 
-    
+    if not os.path.isdir(log_dir):
+      log_setup_info.append((logging.WARNING, 'Log directory missing'))
 
-    for log in log_setup_info:
-      logging.log(level=log[0], msg=log[1])
+      os.mkdir(log_dir)
+      log_setup_info.append((logging.INFO, f'Successfully created log directory at \'{log_dir}\''))
 
-    logging.info('Successfully setup logger.')
+    with open(log_file, 'w') as f:
+      log_setup_info.append((logging.INFO, f'Successfully created log file at \'{log_file}\''))
 
-  except Exception as e:
-    print(f'Logging setup failed:\n{e}')
-    exit(1)
+
+
+  logging.basicConfig(
+    filename=log_file, 
+    level=logging.INFO, 
+    filemode='w',
+    format='%(asctime)s | %(levelname)s:\n\t%(message)s'
+  )
+
+  
+
+  for log in log_setup_info:
+    logging.log(level=log[0], msg=log[1])
+
+  logging.info('Successfully setup logger.')
+
+except Exception as e:
+  print(f'Logging setup failed:\n{e}')
+  exit(1)
