1af8c20f604a977ec0e0b5e08bdc59c4fb37dc24
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-12-04 22:17:59 -0800

    Added a Check for Attributes with No Variation

--- dataHandler/cleanData.py
+++ dataHandler/cleanData.py
@@ -16,28 +16,37 @@ def cleanData(df):
 
   if hasNull(df):
 
+    INITIAL_COL = df.shape[1]
+    INITIAL_ROWS = df.shape[0]
+
+
+
     # Drops attributes with more than 1/3 null values
     THRESHOLD: int = df.shape[0] - (df.shape[0] / 3)
     logging.info(f'Dropping columns with less than {THRESHOLD} valid values...')
-    INITIAL_COL = df.shape[1]
 
     df = df.dropna(axis=1, thresh=THRESHOLD)
-
-    COL_DROPPED = INITIAL_COL - df.shape[1]
-    logging.info(f'Successfully dropped {COL_DROPPED} columns...')
-
-
 
 
 
     # Drops all rows with ANY null values
     logging.info('Dropping any remaining rows with null values...')
-    INITIAL_ROWS = df.shape[0]
 
     df = df.dropna(axis=0)
 
+
+
+    # Drops any attributes without any variation
+    constant_cols = [col for col in df.columns if df[col].nunique() <= 1]
+    logging.info(f'Dropping constant columns:\n\t\t{constant_cols}')
+
+    df = df.drop(columns=constant_cols)
+
+
+
+    COL_DROPPED = INITIAL_COL - df.shape[1]
     ROWS_DROPPED = INITIAL_ROWS - df.shape[0]
-    logging.info(f'Successfully dropped {ROWS_DROPPED} rows...')
+    logging.info(f'Successfully dropped {COL_DROPPED} columns & {ROWS_DROPPED} rows...')
 
   logging.info(f'Final shape of dataframe after cleaning: [{df.shape[0]} Rows, {df.shape[0]} Columns]')
 
