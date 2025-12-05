3730c66fad802e0ca2b7769afb8ab924318381fd
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-12-04 21:36:00 -0800

    Prints Message with Path to Logs
    Also refactored some variables

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,33 +1,35 @@
 import logging
 import os
+from pathlib import Path
 
 
 
 
 
 try:
-  log_dir = '../logs'
-  log_file = '../logs/app.log'
+  LOG_DIR = '../logs/'
+  LOG_FILE = 'app.log'
+  LOG_PATH = os.path.join(LOG_DIR, LOG_FILE)
   log_setup_info = []
 
 
 
-  if not os.path.isfile(log_file):
+  if not os.path.isfile(LOG_PATH):
     log_setup_info.append((logging.WARNING, 'Log file missing'))
 
-    if not os.path.isdir(log_dir):
+    if not os.path.isdir(LOG_DIR):
       log_setup_info.append((logging.WARNING, 'Log directory missing'))
 
-      os.mkdir(log_dir)
-      log_setup_info.append((logging.INFO, f'Successfully created log directory at \'{log_dir}\''))
+      os.mkdir(LOG_DIR)
+      log_setup_info.append((logging.INFO, f'Successfully created log directory at \'{LOG_DIR}\''))
 
-    with open(log_file, 'w') as f:
-      log_setup_info.append((logging.INFO, f'Successfully created log file at \'{log_file}\''))
+    with open(LOG_PATH, 'w') as f:
+      log_setup_info.append((logging.INFO, f'Successfully created log file at \'{LOG_PATH}\''))
 
 
 
   logging.basicConfig(
-    filename=log_file, 
+    filename=LOG_PATH, 
     level=logging.INFO, 
     filemode='w',
     format='%(asctime)s | %(levelname)s:\n\t%(message)s'
@@ -39,6 +41,7 @@ try:
     logging.log(level=log[0], msg=log[1])
 
   logging.info('Successfully setup logger.')
+  print(f'Read logs for full information at {(Path.cwd() / LOG_PATH).resolve()}')
 
 except Exception as e:
   print(f'Logging setup failed:\n{e}')
